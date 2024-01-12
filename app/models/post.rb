a4fd7d08c9cadf0828a9e6236c09b5149af87678
BiancaMe <bianquis0410@gmail.com>
2024-01-12 10:54:35 -0300

    Add validations, run db:reset for problems with migrations. Create tests in rspec

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,15 @@
+require 'rails_helper'
+
 class Post < ApplicationRecord
   has_many :comments
   has_many :likes
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   after_create :increment_counter
   after_destroy :decrement_counter
+
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def recents(limit = 5)
     comments.order(created_at: :desc).limit(limit)
